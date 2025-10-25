a5d19480def211ecfd88288adb880999301bfce8
Sevin Nasiri <sevinnasiri85@gmail.com>
2025-10-25 22:30:59 +0330

    E_6_1_a

---
+++ E_6_1_a.java
@@ -0,0 +1,11 @@
+public class E_6_1_a {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 2; i <= 100; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("result is:" + sum);
+    }
+}
