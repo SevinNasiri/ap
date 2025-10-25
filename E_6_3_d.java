50f2ba4e55b9eabad849cc6925ec064e2514aed7
Sevin Nasiri <sevinnasiri85@gmail.com>
2025-10-25 22:41:32 +0330

    E_6_3

---
+++ E_6_3_d.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class E_6_3_d {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a string:");
+        String input = scanner.nextLine();
+
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if ("aeiou".indexOf(c) != -1){
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
